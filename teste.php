f29283b7223f60abc62bf10f9bded2897cc6005b
Viviam Lopes Rodrigues <viviam.rodrigues@oi.net.br>
2020-03-02 14:29:36 -0300

    fist commit

---
+++ teste.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Calculadora</title>
+
+    <!--  <link rel="stylesheet" href="css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- <script src="js/bootstrap.min.js"></script>-->
+    <!-- <script src="js/bootstrap.bundle.min.js"></script>-->
+    <!--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+</head>
+<body>
+<?php echo "Teste de php";
+echo '<br>';
+$var = "isto é uma variavel";
+echo $var;
+?>
+
+</body>
+</html>
